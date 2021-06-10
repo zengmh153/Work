0234eea6da88b5e688e7f6f3681cbac25e02bfd9
zeng <2363187203@qq.com>
2021-06-10 16:55:35 +0800

    4

---
+++ app/src/main/java/com/text/work/Resou.java
@@ -0,0 +1,52 @@
+package com.text.work;
+
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Resou implements Runnable {
+    private static final String TAG = "Resou";
+    Handler handler;
+
+    public void setHandler(Handler handler) {
+        this.handler = handler;
+    }
+
+    @Override
+    public void run() {
+        ArrayList<HashMap<String,String>> listItems=new ArrayList<HashMap<String,String>>();
+        try {
+            Thread.sleep(500);
+            Document doc = Jsoup.connect(" https://www.boc.cn/sourcedb/whpj/ ").get();
+
+            Element table = doc.getElementsByTag("table").get(1);
+            Elements trs = table.getElementsByTag("td");
+            Log.i(TAG, "run: trs="+trs);
+            for(int i = 0 ;i <trs.size(); i+=8){
+                Element td1 = trs.get(i);
+                Element td2=trs.get(i+5);
+                String a=td1.text();
+                String b=td2.text();
+                HashMap<String,String> map=new HashMap<String, String>();
+                map.put("ItemTitle",a);
+                map.put("ItemDetail",b);
+                listItems.add(map);}
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            Log.e(TAG, "run:" + e.toString());
+        }
+        Message msg = handler.obtainMessage(9, listItems);
+        handler.sendMessage(msg);
+
+    }
+}
+
--- app/src/main/java/com/text/work/page1.java
+++ app/src/main/java/com/text/work/page1.java
@@ -1,17 +1,93 @@
 package com.text.work;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 
-public class page1 extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class page1 extends AppCompatActivity implements AdapterView.OnItemClickListener, AdapterView.OnItemLongClickListener{
+    private static final String TAG = "page1";
+    ListView listView;
+    MyAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page1);
 
         Intent intent=getIntent();
+        listView = findViewById(R.id.mylist1);
+        ProgressBar progressBar = findViewById(R.id.progressBar1);
+
+        Handler handler = new Handler(){
+            @Override
+            public void handleMessage(@NonNull Message msg) {
+                if(msg.what==9){
+                    ArrayList<HashMap<String,String>> retList=(ArrayList<HashMap<String,String>>)msg.obj;
+                    /*
+                    SimpleAdapter adapter=new SimpleAdapter(MyList3Activity.this,retList,R.layout.list_item,
+                            new String[]{"ItemTitle","ItemDetail"},
+                            new int[]{R.id.itemTitle,R.id.itemDetail}
+                            );*/
+
+                    adapter = new MyAdapter(page1.this,R.layout.list_item,retList);
+                    listView.setAdapter(adapter);
+                    //切换显示
+                    progressBar.setVisibility(View.GONE);
+                    listView.setVisibility(View.VISIBLE);
+                }
+                super.handleMessage(msg);
+            }
+        };
+
+        listView.setOnItemClickListener(this);
+        listView.setOnItemLongClickListener(this);
+
+        Resou task=new Resou();
+        task.setHandler(handler);
+        Thread t =new Thread(task);
+        t.start();
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Object itemAtPosition=listView.getItemAtPosition(position);
+        HashMap<String,String> map = (HashMap<String,String>) itemAtPosition;
+        String titleStr = map.get("ItemTitle");
+        String detailStr = map.get("ItemDetail");
+        Log.i(TAG, "onItemClick: titleStr="+titleStr);
+        Log.i(TAG, "onItemClick: detailStr="+detailStr);
+
+    }
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+        Log.i(TAG, "onItemLongClick: 长按事件处理position="+position);
+
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("提示")
+                .setMessage("是否删除数据")
+                .setPositiveButton("是",new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //删除内容
+                        adapter.remove(listView.getItemAtPosition(position));
+                    }
+                }).setNegativeButton("否",null);
+        builder.create().show();
+        return true;
     }
 }
