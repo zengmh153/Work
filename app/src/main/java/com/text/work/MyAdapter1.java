d2d5b9ed226f3ed7b5da8220ef74c3aa65c052fb
zeng <2363187203@qq.com>
2021-06-25 00:48:02 +0800

    14

---
+++ app/src/main/java/com/text/work/MyAdapter1.java
@@ -0,0 +1,57 @@
+package com.text.work;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class MyAdapter1 extends BaseAdapter {
+    List<costList> mList;
+
+    public ListAdapter(List<costList> list) {
+        mList = list;
+    }
+
+    @Override
+    public int getCount() {
+        return mList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = mLayoutInflater.inflate(R.layout.list_item, null);
+        //取出数据赋值
+        costList item = mList.get(position);
+        TextView tv_title = view.findViewById(R.id.tv_title);
+        TextView tv_date = view.findViewById(R.id.tv_date);
+        TextView tv_money = view.findViewById(R.id.tv_money);
+        //绑定
+        tv_title.setText(mList.get(position).getTitle());
+        tv_date.setText(mList.get(position).getDate());
+        tv_money.setText(mList.get(position).getMoney());
+        return view;
+
+    }
+
+    private List<costList> getmList;
+    private LayoutInflater mLayoutInflater;
+
+    public ListAdapter(Context context, List<costList> list) {
+        mList = list;
+        //通过外部传来的Context初始化LayoutInflater对象
+        mLayoutInflater = LayoutInflater.from(context);
+    }
+}
