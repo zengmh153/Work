6af5f3d6947fae1eed91f4f2fb91eff107b27dfb
zeng <2363187203@qq.com>
2021-06-25 00:27:06 +0800

    13

---
+++ app/src/main/java/com/text/work/page3.java
@@ -0,0 +1,14 @@
+package com.text.work;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class page3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page3);
+    }
+}
